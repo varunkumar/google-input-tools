b02da10b16a0b32fdcd3108889c4c7cea19fa6da
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2017-10-25 23:35:14 +0530

    Add basic testsss

--- test/extension.test.js
+++ test/extension.test.js
@@ -13,11 +13,15 @@ const assert = require('assert');
 const vscode = require('vscode'); // eslint-disable-line
 const myExtension = require('../src/extension'); // eslint-disable-line
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite('Extension Tests', () => {
-  // Defines a Mocha unit test
-  test('Something 1', () => {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    assert.equal(-1, [1, 2, 3].indexOf(0));
+suite('Configuration', () => {
+  const Configuration = require('../src/Configuration'); // eslint-disable-line
+  test('language code', () => {
+    assert.notEqual(Configuration.getLanguageCode(), null);
+    assert.notEqual(Configuration.getLanguageCode(), undefined);
+  });
+
+  test('suggestions count', () => {
+    assert.notEqual(Configuration.getSuggestionsCount(), null);
+    assert.notEqual(Configuration.getSuggestionsCount(), undefined);
   });
 });
