341f2ce5f295c59b71577267889bd803930c4045
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2022-01-23 19:03:59 +0530

    Fix the import of node-fetch

--- src/InputAutoCompletionProvider.js
+++ src/InputAutoCompletionProvider.js
@@ -1,5 +1,6 @@
 const vscode = require('vscode'); // eslint-disable-line
-const fetch = require('node-fetch');
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args)); // eslint-disable-line
 const Configuration = require('./Configuration');
 
 const getSearchText = (lineText, position) => {
